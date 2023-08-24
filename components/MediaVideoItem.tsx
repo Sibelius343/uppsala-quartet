e22a58d1cdbc1b06901df6ec8a126a7a4e72d4e1
Samuel Schoenwald <schoenwald343@gmail.com>
2023-08-24 10:54:12 -0500

    removed video description formatting -- was hanging the rendering of the page

--- components/MediaVideoItem.tsx
+++ components/MediaVideoItem.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import { memo, useEffect, useRef, useState } from "react";
+import { memo } from "react";
 import useYoutubeVideoData from "../hooks/useYoutubeVideoData";
 
 interface MediaVideoItemProps {
@@ -8,16 +8,17 @@ interface MediaVideoItemProps {
 
 const MediaVideoItem = memo(({ embedId }: MediaVideoItemProps) => {
   const { data: video } = useYoutubeVideoData(embedId);
-  const formattedDescription = video?.snippet.description
-    // .replace(/\n{2,}/g, '\n')
-    // .replace(/(https?:\/\/)?(w{3}\.)?\w+(\.\w{2,3})(\/\w*)*/gi, "");
+  // const formattedDescription = video?.snippet.description
+  //   .replace(/\n{2,}/g, '\n')
+  //   .replace(/(https?:\/\/)?(w{3}\.)?\w+(\.\w{2,3})(\/\w*)*/gi, "");
+  
   return (
     <Box
       display="flex"
       gap={2}
       flexDirection={{ xs: "column", md: "row" }}
       alignItems={{ xs: "center", md: "start"}}
-      width="80vw"
+      width="90vw"
       overflow="hidden"
     >
       <Box
@@ -38,8 +39,8 @@ const MediaVideoItem = memo(({ embedId }: MediaVideoItemProps) => {
         <Typography variant="h4" fontSize={28} textAlign={{ xs: "center", md: "start"}}>
           {video?.snippet.title}
         </Typography>
-        <Typography overflow="hidden" variant="body2" sx={{ whiteSpace: 'pre-line', textAlign: 'start', alignSelf: { xs: 'center', md: 'start'} }}>
-        {formattedDescription}
+        <Typography overflow="hidden" textOverflow="ellipsis" variant="body2" sx={{ whiteSpace: 'pre-line', textAlign: 'start', alignSelf: { xs: 'center', md: 'start'}, maxHeight: 225 }}>
+        {video?.snippet.description}
         </Typography>
       </Box>
     </Box>

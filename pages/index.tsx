4df0a27be97897453efd2aac144e244c138426d0
Samuel Schoenwald <schoenwald343@gmail.com>
2023-09-29 22:28:35 -0500

    added mount check on index to prevent wrong background image from flashing

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,18 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { Box, useMediaQuery, useTheme } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 const Home: NextPage = () => {
   const theme = useTheme()
   const smallScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const [mounted, setMounted] = useState(false);
 
   const logoDimension = 200;
+
+  useEffect(() => {
+    setMounted(true);
+  }, [])
 
   return (
     <>
@@ -27,14 +33,14 @@ const Home: NextPage = () => {
         sx={{ backgroundColor: 'lightgrey', mt: -2 }}
       >
         <Box position="fixed" width="100vw" height="100vh" top={0}>
-          <Image src={smallScreen ? "/catena-mobile-background.jpg" : "/quartet-collage.jpg"} alt='Catena background' fill style={{ objectFit: "cover" }} />
+          {mounted && <Image src={smallScreen ? "/catena-mobile-background.jpg" : "/quartet-collage.jpg"} alt='Catena background' fill style={{ objectFit: "cover" }} />}
         </Box>
         <Box
           display="flex"
           position={smallScreen ? "static" : "absolute"}
           top="calc(100vh - 200px)"
           left={0}>
-        <Image src={'/catena-logo.png'} alt='Catena logo' height={logoDimension} width={logoDimension} style={{ borderRadius: smallScreen ? "1000px" : "0px 4px 0px 0px" }} />
+        {mounted && <Image src={'/catena-logo.png'} alt='Catena logo' height={logoDimension} width={logoDimension} style={{ borderRadius: smallScreen ? "1000px" : "0px 4px 0px 0px", zIndex: 2 }} priority />}
         </Box>
       </Box>
     </>

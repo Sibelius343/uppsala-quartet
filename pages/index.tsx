0a295ebdc60b9c56a81e72bd1b60744f1434ad70
Samuel Schoenwald <schoenwald343@gmail.com>
2023-11-01 17:30:40 -0500

    fix for fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import { useEffect, useState } from 'react';
 const Home: NextPage = () => {
   const theme = useTheme();
   const [mounted, setMounted] = useState(false);
-  const smallScreen = useMediaQuery(theme.breakpoints.down('md')) || (mounted && window.innerWidth / window.innerHeight > 1.1227);
+  const smallScreen = useMediaQuery(theme.breakpoints.down('md')) || (mounted && window.innerWidth / window.innerHeight < 1.1227);  
 
   const logoDimension = smallScreen ? 350 : 250;
 
